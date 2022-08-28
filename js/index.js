75b62ab499df405ae455abf9dfa7bab883859c96
Gaurav Prasanna <gpece2001@gmail.com>
2022-08-28 09:45:47 +0530

    style commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+/* ========== Typing animation ============== */
+
+var typed = new Typed(".typing", {
+    strings:["","","Data Science Enthusiast", "Web and App Developer", "Astrophile", "Cinephile"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop : true
+})
